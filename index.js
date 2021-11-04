9dc759ac3b2d367454d0d4ff4462d2f08e9495ca
Candice <candice.vernacchio@gmail.com>
2021-11-04 17:34:41 -0600

    commit tree 99b4f751c6854cbdb8d1a47e15e6ea305f38ed80

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // // Code your solution here
 //-----------------------------
 //Correct
-// const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator})
-// console.log(totalBatteries)
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator})
+console.log(totalBatteries)
 // ==========================
-//This is off of MDN... What am I doing wrong?
+// //This is off of MDN... What am I doing wrong?
+// const totalBatteries = (previousValue, currentValue) => previousValue + currentValue;
 
-const totalBatteries = (previousValue, currentValue) => previousValue + currentValue
-batteryBatches.reduce(totalBatteries)
+// console.log(batteryBatches.reduce(totalBatteries));
