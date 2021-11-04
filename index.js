ea9392a4ffa7117c13aac47ecf6a64122193376f
Candice <candice.vernacchio@gmail.com>
2021-11-04 16:46:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// // Code your solution here
+//-----------------------------
+//Correct
+// const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator})
+// console.log(totalBatteries)
+// ==========================
+//This is off of MDN... What am I doing wrong?
 
-// Code your solution here
+const totalBatteries = (previousValue, currentValue) => previousValue + currentValue
+return batteryBatches.reduce(totalBatteries)
